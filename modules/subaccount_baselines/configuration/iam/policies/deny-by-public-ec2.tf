7f33126e8727069cf684f7bb6783107683060cf2
Majin Buu <majinbuuonsec@gmail.com>
2024-01-26 16:08:15 -0800

    del more

--- modules/subaccount_baselines/configuration/iam/policies/deny-by-public-ec2.tf
+++
@@ -1,5 +0,0 @@
-# ec2:AssociateIamInstanceProfile
-# ec2:AttachNetworkInterface
-# ec2:StartInstances
-
-# resource = all public EC2s
--- modules/subaccount_baselines/configuration/iam/policies/deny-by-public-eks.tf
+++
@@ -1,6 +0,0 @@
-# data "aws_eks_clusters" "example" {}
-
-# data "aws_eks_cluster" "example" {
-#   for_each = toset(data.aws_eks_clusters.example.names)
-#   name     = each.value
-# }
